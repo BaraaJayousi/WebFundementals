23f3c0279171e48d8c22b706b60e4cb23099c807
Baraa Jayousi <b.jayousi98@gmail.com>
2024-01-26 10:51:40 +0200

    applied multiple events handlers

--- Advanced Topics/jquery/script.js
+++ Advanced Topics/jquery/script.js
@@ -1,3 +1,31 @@
-$("button").click(()=>{
-    alert("You have clicked button event")
-})
+//ensuring jquery doens't start before the document is ready
+//Below line is a short version for $(document).ready(function(){}
+$(()=>{
+    $("button").click(()=>{
+        // alert("You have clicked button event")
+        $("#hover").show();
+    })
+    
+    $("#hover").mouseleave(function() {
+        //use "this" to select same element's selected
+        $(this).fadeOut(300);
+    })
+    
+
+    $("#multi-event").on({
+        mouseenter: function(){
+            $(this).css("background-color", "lightgray");
+        },
+        mouseleave: function(){
+            $(this).css("background-color", "lightblue");
+        },
+        click: function(){
+            $(this).css("background-color", "yellow");
+        }
+    });
+
+    $("#txt-change").dclick(function(){
+        $(this)
+    })
+});
+
