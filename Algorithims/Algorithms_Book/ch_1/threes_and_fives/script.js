3f0b5649d3b78bc4f93af279a826392b61a6053d
Baraa Jayousi <b.jayousi98@gmail.com>
2024-01-31 19:48:36 +0200

    solved Threes and Fives challenge

--- Algorithims/Algorithms_Book/ch_1/threes_and_fives/script.js
+++ Algorithims/Algorithms_Book/ch_1/threes_and_fives/script.js
@@ -1,11 +1,24 @@
 function threesAndFives(){
+    let sum = 0;
     for(let i= 100; i <= 400000;i++){
+        let divBy3 = i%3 == 0
+        let divBy5 = i%5 == 0
+        if((divBy3 || divBy5) && !(divBy3 && divBy5)){
+            sum+= i;
+        }
+    }
+    return sum;
+}
+
+function betterThreesAndFives(start, end){
+    let sum = 0;
+    for(let i= start; i <= end;i++){
         let divBy3 = i%3 == 0
         let divBy5 = i%5 == 0
         
         if((divBy3 || divBy5) && !(divBy3 && divBy5)){
-            if(divBy3 && divBy5)
-                console.log("Number: "+i+ " 3: "+divBy3 +"  5: "+divBy5)
+            sum+= i;
         }
     }
+    return sum;
 }
