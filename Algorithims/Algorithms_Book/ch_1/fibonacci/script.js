b8f45ab7d7ec72ce8dfda3cb536b7d94d5494155
Baraa Jayousi <b.jayousi98@gmail.com>
2024-02-01 07:01:13 +0200

    Implemented fibonacci function using for loop

--- Algorithims/Algorithms_Book/ch_1/fibonacci/script.js
+++ Algorithims/Algorithms_Book/ch_1/fibonacci/script.js
@@ -1,10 +1,30 @@
 //used recusions for faster implementation
+// function fibonacci(num){
+//     if(num == 0){
+//         return 0
+//     }else if (num == 1 || num == 2){
+//         return 1
+//     }
+
+//     return fibonacci(num - 1) + fibonacci(num - 2)
+// }
+
+
+//used loops for loop training 
 function fibonacci(num){
     if(num == 0){
         return 0
     }else if (num == 1 || num == 2){
         return 1
     }
+    let fibonacciNum = 1
+    let n1 = 1;
+    let n2 = 1;
+    for(let i = 3; i <= num; i++){
+        fibonacciNum = n1 + n2;
+        n2 = n1;
+        n1 = fibonacciNum;
+    }
 
-    return fibonacci(num - 1) + fibonacci(num - 2)
+    return fibonacciNum;
 }
