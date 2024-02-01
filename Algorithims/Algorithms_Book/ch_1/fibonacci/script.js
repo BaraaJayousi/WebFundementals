6b552cd16e1bd55f0d4a5d1385bcf84d01051563
Baraa Jayousi <b.jayousi98@gmail.com>
2024-02-01 06:42:51 +0200

    implemented fibonacci function with recursions

---
+++ Algorithims/Algorithms_Book/ch_1/fibonacci/script.js
@@ -0,0 +1,10 @@
+//used recusions for faster implementation
+function fibonacci(num){
+    if(num == 0){
+        return 0
+    }else if (num == 1 || num == 2){
+        return 1
+    }
+
+    return fibonacci(num - 1) + fibonacci(num - 2)
+}
